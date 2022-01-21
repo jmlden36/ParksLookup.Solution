8cff7188b9110ff8563c8d1649f6e7bcb7b002be
Matt Luker <jamesmattluker@gmail.com>
2022-01-21 10:16:08 -0700

    add ParksController class to ParksController.cs

---
+++ ParksLookup/Controllers/ParksController.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CretaceousPark.Models;
+
+namespace ParksLookup.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class ParksController : ControllerBase
+  {
+    private readonly ParksLookupContext _db;
+    
+    public ParksController(ParksLookupContext db)
+    {
+      _db = db;
+    }
+  }
+}
